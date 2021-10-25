b5c6742ba9785286b3f9a6b9b8e9426fddc923d9
chromecast56 <51351043+chromecast56@users.noreply.github.com>
2021-10-25 11:40:48 -0600

    test

---
+++ 2017MilkMeasurementSilver/measurement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class measurement {
+    public static void main(String[] args) throws IOException {
+        System.out.println("hello world");
+    }
+}
