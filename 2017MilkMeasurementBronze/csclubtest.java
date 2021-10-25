f461136b8a98f084528107e841a14c9890ff73ed
chromecast56 <51351043+chromecast56@users.noreply.github.com>
2021-10-25 11:26:30 -0600

    finished solution

--- 2017MilkMeasurementBronze/csclubtest.java
+++ 2017MilkMeasurementBronze/csclubtest.java
@@ -1,9 +1,45 @@
 import java.util.*;
 import java.io.*;
 public class csclubtest {
-    static Scanner sc = new Scanner(System.in);
+    static class Kattio extends PrintWriter {
+		private BufferedReader r;
+		private StringTokenizer st;
+	
+		// standard input
+		public Kattio() { this(System.in, System.out); }
+		public Kattio(InputStream i, OutputStream o) {
+			super(o);
+			r = new BufferedReader(new InputStreamReader(i));
+		}
+		// USACO-style file input
+		public Kattio(String problemName) throws IOException {
+			super(new FileWriter(problemName + ".out"));
+			r = new BufferedReader(new FileReader(problemName + ".in"));
+		}
+	
+		// returns null if no more input
+		public String next() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(r.readLine());
+				return st.nextToken();
+			} catch (Exception e) { }
+			return null;
+		}
+	
+		public int nextInt() { return Integer.parseInt(next()); }
+		public double nextDouble() { return Double.parseDouble(next()); }
+		public long nextLong() { return Long.parseLong(next()); }
+	}
+	
+	static Kattio io;
+	static {
+		try {
+			io = new Kattio("measurement");
+		} catch(IOException e) {}
+	}
 	public static void main(String[] args) {
-        int n = sc.nextInt(); //input a from the user
+        int n =io.nextInt(); //input a from the user
         boolean[] is_max = new boolean[3];
         is_max[0] = is_max[1] = is_max[2] = true;
 
@@ -15,9 +51,9 @@ public class csclubtest {
         int[] edit_who = new int[101];
 
         for (int i=0;i<n;i++) {
-            int day = sc.nextInt();
-            String name = sc.next();
-            int delta = sc.nextInt();
+            int day = io.nextInt();
+            String name = io.next();
+            int delta = io.nextInt();
 
             edit_val[day] = delta;
             if (name.equals("Bessie"))
@@ -36,8 +72,20 @@ public class csclubtest {
             boolean[] next_max = new boolean[3];
             for (int j=0;j<3;j++) {
                 if (vals[j]==max)
-                next_max[j] = true
+                    next_max[j] = true;
             }
+
+            for (int j=0;j<3;j++)
+                if (is_max[j]!=next_max[j]) {
+                    num_changes++;
+                    break;
+                }
+
+            for (int j=0;j<3;j++)
+                is_max[j] = next_max[j];
         }
+
+        io.println(num_changes);
+        io.close();
 	}
 }
